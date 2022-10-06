684f841f7f8eb50b139e129ce8d1fa52b614a8ba
alexb <v4004462@gmail.com>
2022-10-06 19:26:54 +0300

    task13

--- Task_13/Program.cs
+++ Task_13/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 13: Напишите программу, которая выводит третью
+// цифру заданного числа или сообщает, что третьей цифры неt
+
+Console.WriteLine("Введите число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+string userNumberString = Convert.ToString(userNumber);
+
+if (userNumberString.Length > 2)
+{
+    Console.WriteLine("Третья цифра: " + userNumberString[2]);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
