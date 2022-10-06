76f328a1ab65685c37494ac23478793dd843e7fc
alexb <v4004462@gmail.com>
2022-10-06 18:45:17 +0300

    task10

--- Task_10/Program.cs
+++ Task_10/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿// Задача 10: Напишите программу, которая принимает на вход
+//трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+string x = Console.ReadLine();
+int userNumber = int.Parse(x);
+int secondNumber = userNumber / 10 % 10;
+if (userNumber>999)
+{
+    Console.WriteLine("error");
+
+}
+else
+{
+    Console.WriteLine(secondNumber);
+}
+
+
+
